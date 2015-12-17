c50bb52a6f03074bc8ca7a7e5b7e81ed898407d8
harshita <s.harshita.89@gmail.com>
2015-12-17 10:54:32 +1030

    initial commit

--- www/js/controller/RiskReportController.js
+++ www/js/controller/RiskReportController.js
@@ -2,20 +2,6 @@ angular.module('starter.riskreportcontroller', ['ionic-datepicker'])
 
 .controller('RiskReportController', function($scope, $http) {
 
- $scope.roles = [
-          {"id": 1, "name": "Manager", "assignable": true},
-          {"id": 2, "name": "Developer", "assignable": true},
-          {"id": 3, "name": "Reporter", "assignable": true}
-    ];
-    
-    $scope.member = {roles: []};
-    $scope.selected_items = [];
-
-/*
-  Dropdown with Multiple checkbox select with jQuery - May 27, 2013
-  (c) 2013 @ElmahdiMahmoud
-  license: http://www.opensource.org/licenses/mit-license.php
-*/ 
 
 
 /*
@@ -24,41 +10,14 @@ angular.module('starter.riskreportcontroller', ['ionic-datepicker'])
   license: http://www.opensource.org/licenses/mit-license.php
 */ 
 
-$(".dropdown dt a").on('click', function () {
-          $(".dropdown dd ul").slideToggle('fast');
-      });
 
-      $(".dropdown dd ul li a").on('click', function () {
-          $(".dropdown dd ul").hide();
-      });
-
-      function getSelectedValue(id) {
-           return $("#" + id).find("dt a span.value").html();
-      }
-
-      $(document).bind('click', function (e) {
-          var $clicked = $(e.target);
-          if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
-      });
+/*
+  Dropdown with Multiple checkbox select with jQuery - May 27, 2013
+  (c) 2013 @ElmahdiMahmoud
+  license: http://www.opensource.org/licenses/mit-license.php
+*/ 
 
 
-      $('.mutliSelect input[type="checkbox"]').on('click', function () {
-        
-          var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-              title = $(this).val() + ",";
-        
-          if ($(this).is(':checked')) {
-              var html = '<span title="' + title + '">' + title + '</span>';
-              $('.multiSel').append(html);
-              $(".hida").hide();
-          } 
-          else {
-              $('span[title="' + title + '"]').remove();
-              var ret = $(".hida");
-              $('.dropdown dt a').append(ret);
-              
-          }
-      });
 
 
 
