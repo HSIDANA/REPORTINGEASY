3d27c247a841c29f85789696721f49ffd2d94dbd
harshita <s.harshita.89@gmail.com>
2016-01-08 10:21:53 +1030

    y

--- www/js/controller/ChatsCtrl.js
+++ www/js/controller/ChatsCtrl.js
@@ -175,6 +175,53 @@ alert("in");
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+
+$scope.selectPhoto = function(){
+    Camera.getPicture({
+        sourceType:0,       //photo album,
+        destinationType:1,  //file URI
+        saveToPhotoAlbum:false,
+        correctOrientation:true
+    }).then(function(imageURI) {
+        $scope.imageURI = imageURI; //THIS ONLY WORKS SOMETIMES
+    }, function(err) {
+        alert('An error has occured');
+    });
+};
+    $scope.ImageURI = 'content://media/external/images/media/17';
+
+            function UploadPicture(imageURI) {
+
+                    $scope.PicSourece  = document.getElementById('smallimage');
+
+
+                    if (imageURI.substring(0,21)=="content://com.android") {
+                        var photo_split=imageURI.split("%3A");
+                        imageURI="content://media/external/images/media/"+photo_split[1];
+
+                    }
+                    $scope.ImageURI =  imageURI;
+                    $scope.PicSourece.src = $scope.ImageURI;
+                    $scope.apply();
+
+
+
+            }
+
+            $scope.ShowPictures = function(){
+                alert("hh2");
+                navigator.camera.getPicture(UploadPicture, function(message) {
+                        alert('get picture failed');
+                        },{
+                        quality: 50,
+                        destinationType: navigator.camera.DestinationType.FILE_URI,
+                        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+                    }
+
+                );
+                $scope.ImageURI = 'Vamsidhar';
+            };
+
   $http.get('js/resources/incidents.js').success(function (results) {
 
     //$scope.categories = results.categories;
