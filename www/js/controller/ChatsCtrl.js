a37a35ddab493d87d87bb056bc4f121a435477c5
harshita <s.harshita.89@gmail.com>
2016-01-08 09:44:00 +1030

    yes

--- www/js/controller/ChatsCtrl.js
+++ www/js/controller/ChatsCtrl.js
@@ -38,14 +38,8 @@ $ionicHistory.clearHistory();
   $scope.getPhoto = function() {
     Camera.getPicture().then(function(imageURI) {
       console.log(imageURI);
-      $scope.lastPhoto = imageURI;
     }, function(err) {
       console.err(err);
-    }, {
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: false
     });
   };
     $scope.upload = function() 
